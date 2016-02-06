6ecc4851fd5642e83ac311d7ea50fce5fdb0928a
Burak <burak@burak.fr>
2016-02-06 21:44:08 +0100

    changed default port on config.ini to allow linux no-root execution

--- test1.php
+++ test1.php
@@ -1,48 +1,55 @@
 <?php
+
 class Thread1 extends Thread {
-	public function __construct(SharedThing $k){
-		$this->k = $k;
-	}
+
+    public function __construct(SharedThing $k) {
+        $this->k = $k;
+    }
 
     public function run() {
-    	while(true){
-        	$this->k->setX();
-        	print("COUNT #1 : ".count($this->k->thing)."\n");
-        	
-        	sleep(1);
-    	}
+        while (true) {
+            $this->k->setX();
+            print("COUNT #1 : " . count($this->k->thing) . "\n");
+
+            sleep(1);
+        }
     }
+
 }
 
 class Thread2 extends Thread {
-	public function __construct(SharedThing $k){
-		$this->k = $k;
-	}
+
+    public function __construct(SharedThing $k) {
+        $this->k = $k;
+    }
 
     public function run() {
-    	while(true){
-        	//$this->k->setX();
-        	$this->k->thing[] = 0;
-        	print("COUNT #2 : ".count($this->k->thing)."\n");
-        	usleep(500000);
-    	}
+        while (true) {
+            //$this->k->setX();
+            $this->k->thing[] = 0;
+            print("COUNT #2 : " . count($this->k->thing) . "\n");
+            usleep(500000);
+        }
     }
+
 }
 
-class SharedThing extends Threaded{
-	public $thing;
+class SharedThing extends Threaded {
 
-	public function __construct(){
-		$this->thing = [];
-		$this->thing[] = 5;
-		$this->thing[] = 4;
-	}
+    public $thing;
 
-	public function setX(){
-		$this->thing[] = 3;
-		$this->thing[] = 2;
-		$this->thing[] = 1;
-	}
+    public function __construct() {
+        $this->thing = [];
+        $this->thing[] = 5;
+        $this->thing[] = 4;
+    }
+
+    public function setX() {
+        $this->thing[] = 3;
+        $this->thing[] = 2;
+        $this->thing[] = 1;
+    }
+
 }
 
 $shared = new SharedThing();

599db9b4109fbf70839d92d986a26fba7fb83f88
Burak <burak@burak.fr>
2016-02-09 17:13:33 +0100

    Stop program if missing pthreads extension

--- main.php
+++ main.php
@@ -6,6 +6,9 @@ ini_set('display_errors', 1);
 if (!gc_enabled())
     gc_enable();
 
+if (!extension_loaded("pthreads"))
+    die("Please install pthreads extension !");
+
 if (file_exists('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 } else {
